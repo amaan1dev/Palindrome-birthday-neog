7e51b30687ab3e4797fe870e8814c061a3752b28
Amaansyed309 <64450603+Amaansyed309@users.noreply.github.com>
2022-09-26 14:17:17 +0530

    update

--- app.js
+++ app.js
@@ -1,73 +1,14 @@
+var inputDate = document.querySelector("#input-date");
+
+var checkButton = document.querySelector("#check-button") 
+
+
 function reverseStr(str){
     var listOfChars = str.split('');
-    var reverListOfChars = listOfChars.reverse();
-    var reversedStr = reverListOfChars.join('');
+    var reverseListOfChars =  listOfChars.reverse();
+    var reversedStr = reverseListOfChars.join('');
+
     return reversedStr;
 }
 
-
-function isPalindrome(str){
-    var reverse = reverseStr(str);
-    return str === reverse;
-}
-
-
-function converDateToStr(date) {
-    
-    var dateStr = {day: '', month: '', year: ''};
-    
-    if(date.day < 10){
-        dateStr.day = '0' + date.day;
-    }
-    else{
-        dateStr.day = date.day.toString();
-    }
-
-    if(date.month < 10){
-        dateStr.month = '0' + date.month;
-    }
-    else{
-        dateStr.month = date.month.toString();
-    }
-
-    dateStr.year = date.year.toString();
-
-    return dateStr;
-
-}
-
-function getAllDateFormats(date){
-    var dateStr = converDateToStr(date);
-
-    var ddmmyyyy = dateStr.day + dateStr.month + dateStr.year;
-    var mmddyyyy = dateStr.month + dateStr.day + dateStr.year;
-    var yyyymmdd = dateStr.year + dateStr.month + dateStr.day
-    var ddmmyy = dateStr.day + dateStr.month + dateStr.year.slice(-2);
-    var mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2);
-    var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.day;
-
-    return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd];
-
-}
- 
-function checkPalindromeForAllDateFormats(date){
-    var listOfPalindromes = getAllDateFormats(date);
-
-    var flag = false;
-
-    for(var i = 0; i < listOfPalindromes.length; i++){
-        if(isPalindrome(listOfPalindromes[i])){
-            flag = true;
-            break; 
-        }
-    }
-    return flag;
-}
-
-var date = {
-    daye: 2,
-    month: 11,
-    year: 2022
-};
-
-console.log(checkPalindromeForAllDateFormats(date));
+console.log(reverseStr('hello'))
