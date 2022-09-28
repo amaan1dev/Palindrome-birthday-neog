d5779b0a52f0e12d89eb726594277a9f9d512644
Amaansyed309 <64450603+Amaansyed309@users.noreply.github.com>
2022-09-28 17:11:48 +0530

    update

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-var inputDate = document.querySelector("#input-date");
-
-var checkButton = document.querySelector("#check-button") 
-
-
 function reverseStr(str){
     var listOfChars = str.split('');
     var reverseListOfChars =  listOfChars.reverse();
@@ -11,4 +6,41 @@ function reverseStr(str){
     return reversedStr;
 }
 
-console.log(reverseStr('hello'))
+function isPalindrome(str){
+   var reverse = reverseStr(str) 
+   return str === reverse;
+}
+
+function convertDateToStr(date){
+
+    var dateStr = { day: '', month: '', year: ''};
+
+    if(date.day < 10){
+        dateStr.day = '0' + date.day;
+    }
+    else {
+        dateStr.day = date.day.toString(); 
+    }
+    if(date.month < 10){
+        dateStr.month = '0' + date.month;
+    }
+    else {
+        dateStr.month = date.month.toString(); 
+    }
+
+    dateStr.year = date.year.toString();
+
+    return dateStr;
+
+}
+ 
+function getAllDateFormats(date){
+    var dateStr = convertDateToStr(date)
+
+    var ddmmyyyy = dateStr.day + dateStr.month + dateStr.year;
+    var mmddyyyy = dateStr.month + dateStr.day + dateStr.year;
+    var yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
+    var ddmmyy = dateStr.day + dateStr.month + dateStr.year.slice(-2);
+    var mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2);
+    var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.day;
+}
