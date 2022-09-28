60e6e6cfe31332f9b924f395d1b58a481ad941b8
Amaansyed309 <64450603+Amaansyed309@users.noreply.github.com>
2022-09-28 22:18:55 +0530

    update

--- app.js
+++ app.js
@@ -43,4 +43,172 @@ function getAllDateFormats(date){
     var ddmmyy = dateStr.day + dateStr.month + dateStr.year.slice(-2);
     var mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2);
     var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.day;
+
+    return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd]
 }
+
+function checkPalindromeForAllDateFormats(date){
+    var listOfPalindromes = getAllDateFormats(date);
+
+    var verifyPalindrome = false;
+
+    for(var i = 0; i < listOfPalindromes.length; i++){
+        if(isPalindrome(listOfPalindromes[i])){
+            verifyPalindrome = true;
+            break;
+        }
+    }
+
+    return verifyPalindrome;
+}
+
+function isLeapYear(year){
+    if(year % 400 === 0){
+        return true;
+    }
+    if(year % 100 === 0){
+        return false;
+    }
+    if(year % 4 === 0){
+        return true;
+    }
+    return false;
+}
+
+function getNextDate(date){
+    var day = date.day + 1;
+    var month = date.month;
+    var year = date.year;
+
+    var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+     if(month === 2){
+        if(isLeapYear(year)){
+          if(day > 29){
+            day = 1;
+            month++;
+          }
+
+        }
+        else{
+          
+            if(day > 28){
+                day = 1;
+                month++;
+            }
+        }
+     }
+     else{
+        if(day > daysInMonth[month - 1]){
+            day = 1;
+            month++;
+        }
+     }
+
+     if(month > 12){
+        month = 1;
+        year++;
+     }
+     
+     return {
+        day: day,
+        month: month,
+        year: year
+     }
+
+}
+
+function getPreviousDate(date){
+
+    var day = date.day - 1;
+    var month = date.month;
+    var year = date.year;
+
+    var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    if (day === 0) {
+        month--;
+    
+        if (month === 0) {
+          month = 12;
+          day = 31;
+          year--;
+        } else if (month === 2) {
+          if (isLeapYear(year)) {
+            day = 29;
+          } else {
+            day = 28;
+          }
+        } else {
+          day = daysInMonth[month - 1];
+        }
+      }
+     
+     return {
+        day: day,
+        month: month,
+        year: year
+     };
+
+}
+
+function getNextPalindromDate(date){
+
+    var ctr = 0;
+    var nextDate = getNextDate(date);
+
+    while(1){
+        ctr++;
+        var isPalindrome = checkPalindromeForAllDateFormats(nextDate);
+        if(isPalindrome){
+            break;
+        }
+        nextDate = getNextDate(nextDate);
+    }
+
+    return [ctr, nextDate];
+
+}
+
+function getPreviousPalindromeDate(date){
+    var ctr = 0;
+    var previousDate = getPreviousDate(date);
+
+    while(1){
+        ctr++;
+        var isPalindrome = checkPalindromeForAllDateFormats(previousDate);
+        if(isPalindrome){
+            break;
+        }
+        previousDate = getPreviousDate(previousDate);
+    }
+
+    return [ctr, previousDate];
+}
+
+var inputBday = document.querySelector("#input-bday");
+var resultText = document.querySelector("#result-text");
+var checkBtn = document.querySelector("#check-btn");
+
+
+function checkBtnClickHandler(e){
+    var bdayStr = inputBday.value;
+
+    if(inputBday !== ''){
+        var listOfDate = bdayStr.split("-");
+        var date = {
+            day: Number(listOfDate[2]),
+            month: Number(listOfDate[1]),
+            year: Number(listOfDate[0])
+        };
+        
+        var isPalindrome = checkPalindromeForAllDateFormats(date);
+
+        if(isPalindrome){
+            resultText.innerText = "Yay it is a Palindrome";
+        }
+        
+    }
+}
+
+checkBtn.addEventListener("click", checkBtnClickHandler)
